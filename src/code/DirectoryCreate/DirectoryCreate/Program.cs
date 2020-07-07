9a0812e3dacb35678ac097450afdf485b5e89a58
daesu.shin <newbigwater@gmail.com>
2020-07-07 21:00:45 +0900

    [cs][sample] 1. DirectoryInfo : 디렉토리 & 파일 정보 조회 2. DirectoryCreate : 디렉토리 & 파일 생성 및 생성 시간 업데이트

---
+++ src/code/DirectoryCreate/DirectoryCreate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryCreate
+{
+    class Program
+    {
+        static void OnWrongPathType(string type)
+        {
+            Console.WriteLine($"{type} is wrong type");
+            return;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Usage : Program.exe <path> [Type:File/Directory]");
+                return;
+            }
+
+            string path = args[0];
+            string type = "File";
+            if (args.Length > 1)
+                type = args[1];
+
+            if(File.Exists(path) || Directory.Exists(path))
+            {
+                if (type == "File")
+                    File.SetLastWriteTime(path, DateTime.Now);
+                else if (type == "Directory")
+                    Directory.SetLastWriteTime(path, DateTime.Now);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                Console.WriteLine($"Updated {path} {type}");
+            }
+            else
+            {
+                if (type == "File")
+                    File.Create(path).Close();
+                else if (type == "Directory")
+                    Directory.CreateDirectory(path);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                Console.WriteLine($"Created {path} {type}");
+            }
+        }
+    }
+}
---
+++ src/code/DirectoryInfo/DirectoryInfo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
+
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine($"- Directories :");
+            var directories = (from dir in Directory.GetDirectories(directory)
+                let info = new System.IO.DirectoryInfo(dir)
+                select new
+                {
+                    name = info.Name,
+                    Attributes = info.Attributes
+                }).ToList();
+
+            foreach(var d in directories)
+                Console.WriteLine($"{d.name} : {d.Attributes}");
+
+            Console.WriteLine("- Files : ");
+            var files = (from file in Directory.GetFiles(directory)
+                let info = new FileInfo(file)
+                select new
+                {
+                    Name = info.Name,
+                    FileSize = info.Length,
+                    Attributes = info.Attributes
+                }).ToList();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.FileSize}, {file.Attributes}");
+            }
+        }
+    }
+}
